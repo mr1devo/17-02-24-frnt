74c715891e528b6d9848511906e9e2d979061601
mr1devo <128217343+mr1devo@users.noreply.github.com>
2024-02-17 00:27:33 -0930

    Add files via upload

---
+++ src/components/adminpanel/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './home.css'
+import Topbar from './Topbar'
+import Sidebar from './Sidebar'
+import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
+import Layout from './Layout';
+import OutlinedCard from './OutlinedCard';
+
+const Home = (props) => {
+  return (
+    <Layout>
+    <div>
+{/* <OutlinedCard></OutlinedCard> */}
+      {/* <Topbar xxx={props.checkLogout}/> */}
+     {/* <h1 className='wel'> Welcome to Admin Panel</h1> */}
+
+     {/* <Sidebar/> */} 
+      </div>
+      </Layout>
+  )
+}
+
+export default Home
